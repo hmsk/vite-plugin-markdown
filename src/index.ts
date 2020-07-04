f6c1b3fda9535f9b0b67d7f3b7b107f493587a6c
hmsk <k.hamasaki@gmail.com>
2020-07-04 00:15:27 -0700

    Implement a quick MVP

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,25 @@
+import frontmatter from "front-matter"
+import markdownIt from "markdown-it"
+import { Transform } from 'vite'
+
+const transform: Transform = {
+  test: ({ path }) => path.endsWith('.md'),
+  transform: ({ code }) => {
+    const fm = frontmatter(code)
+    const html = markdownIt({ html: true }).render(fm.body)
+    return {
+      code: `const things = {
+               attributes: ${JSON.stringify(fm.attributes)},
+               html: ${JSON.stringify(html)}
+             }
+             export default things
+            `
+    }
+  }
+}
+
 module.exports = (options = {}) => {
   return {
-    resolvers: [],
-    transforms: []
+    transforms: [transform]
   }
 }
