0549a4ed894e608e43733f422b035aad9d7ac2ad
hmsk <k.hamasaki@gmail.com>
2020-07-05 18:16:53 -0700

    Support markdown, markdownIt options as same as frontmatter-markdown-loader

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,46 @@
-import frontmatter from "front-matter"
-import markdownIt from "markdown-it"
+import Frontmatter from 'front-matter'
+import MarkdownIt from 'markdown-it'
 import { Transform } from 'vite'
 
-const transform: Transform = {
-  test: ({ path }) => path.endsWith('.md'),
-  transform: ({ code }) => {
-    const fm = frontmatter(code)
-    const html = markdownIt({ html: true }).render(fm.body)
-    return {
-      code: `const things = {
-               attributes: ${JSON.stringify(fm.attributes)},
-               html: ${JSON.stringify(html)}
-             }
-             export default things
-            `
+export interface PluginOptions {
+  markdownIt?: MarkdownIt | MarkdownIt.Options
+  markdown?: (body: string) => string
+}
+
+const markdownCompiler = (options: PluginOptions): MarkdownIt | { render: (body: string) => string } => {
+  if (options.markdownIt) {
+    if (options.markdownIt instanceof MarkdownIt || (options.markdownIt?.constructor?.name === 'MarkdownIt')) {
+      return options.markdownIt as MarkdownIt
+    } else if (typeof options.markdownIt === 'object') {
+      return MarkdownIt(options.markdownIt)
+    }
+  } else if (options.markdown) {
+    return { render: options.markdown }
+  }
+  return MarkdownIt({ html: true })
+}
+
+const transform = (options: PluginOptions): Transform => {
+  return {
+    test: ({ path }) => path.endsWith('.md'),
+    transform: ({ code }) => {
+      const fm = Frontmatter(code)
+
+      const html = markdownCompiler(options).render(fm.body)
+      return {
+        code: `const things = {
+                attributes: ${JSON.stringify(fm.attributes)},
+                html: ${JSON.stringify(html)}
+              }
+              export default things
+              `
+      }
     }
   }
 }
 
-module.exports = (options = {}) => {
+module.exports = (options: PluginOptions = {}) => {
   return {
-    transforms: [transform]
+    transforms: [transform(options)]
   }
 }
