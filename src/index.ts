f0c04c835ca63d3f32e932705f60285bb9f66757
hmsk <k.hamasaki@gmail.com>
2021-01-05 22:41:34 -0800

    Support vite@2 with updating all dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import Frontmatter from 'front-matter'
 import MarkdownIt from 'markdown-it'
-import { Transform, Plugin } from 'vite'
+import { Plugin } from 'vite'
+import { TransformResult } from 'rollup'
 import { parseDOM, DomUtils } from 'htmlparser2'
 import { Element, Node as DomHandlerNode } from 'domhandler'
 
@@ -47,121 +48,122 @@ class ExportedContent {
   }
 }
 
-const transform = (options: PluginOptions): Transform => {
-  return {
-    test: ({ path }) => path.endsWith('.md'),
-    transform: ({ code, path, id }) => {
-      const content = new ExportedContent()
-      const fm = Frontmatter<unknown>(code)
-      content.addContext(`const attributes = ${JSON.stringify(fm.attributes)}`)
-      content.addExporting('attributes')
+const tf = (code: string, id: string, options: PluginOptions): TransformResult => {
+  if (!id.endsWith('.md')) return null
 
-      const html = markdownCompiler(options).render(fm.body)
-      if (options.mode?.includes(Mode.HTML)) {
-        content.addContext(`const html = ${JSON.stringify(html)}`)
-        content.addExporting('html')
-      }
+  const content = new ExportedContent()
+  const fm = Frontmatter<unknown>(code)
+  content.addContext(`const attributes = ${JSON.stringify(fm.attributes)}`)
+  content.addExporting('attributes')
 
-      if (options.mode?.includes(Mode.TOC)) {
-        const root = parseDOM(html)
-        const indicies = root.filter(
-          rootSibling => rootSibling instanceof Element && ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'].includes(rootSibling.tagName)
-        ) as Element[]
+  const html = markdownCompiler(options).render(fm.body)
+  if (options.mode?.includes(Mode.HTML)) {
+    content.addContext(`const html = ${JSON.stringify(html)}`)
+    content.addExporting('html')
+  }
 
-        const toc: { level: string; content: string }[] = indicies.map(index => ({
-          level: index.tagName.replace('h', ''),
-          content: DomUtils.getInnerHTML(index)
-        }))
+  if (options.mode?.includes(Mode.TOC)) {
+    const root = parseDOM(html)
+    const indicies = root.filter(
+      rootSibling => rootSibling instanceof Element && ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'].includes(rootSibling.tagName)
+    ) as Element[]
 
-        content.addContext(`const toc = ${JSON.stringify(toc)}`)
-        content.addExporting('toc')
-      }
+    const toc: { level: string; content: string }[] = indicies.map(index => ({
+      level: index.tagName.replace('h', ''),
+      content: DomUtils.getInnerHTML(index)
+    }))
 
-      if (options.mode?.includes(Mode.REACT)) {
-        const root = parseDOM(html, { lowerCaseTags: false })
-        const subComponentNamespace = 'SubReactComponent'
+    content.addContext(`const toc = ${JSON.stringify(toc)}`)
+    content.addExporting('toc')
+  }
 
-        const markCodeAsPre = (node: DomHandlerNode): void => {
-          if (node instanceof Element) {
-            if (node.tagName.match(/^[A-Z].+/)) {
-              node.tagName = `${subComponentNamespace}.${node.tagName}`
-            }
-            if (['pre', 'code'].includes(node.tagName) && node.attribs?.class) {
-              node.attribs.className = node.attribs.class
-              delete node.attribs.class
-            }
+  if (options.mode?.includes(Mode.REACT)) {
+    const root = parseDOM(html, { lowerCaseTags: false })
+    const subComponentNamespace = 'SubReactComponent'
 
-            if (node.tagName === 'code') {
-              const codeContent = DomUtils.getInnerHTML(node)
-              node.attribs.dangerouslySetInnerHTML = `vfm{{ __html: \`${codeContent.replace(/([\\`])/g, "\\$1")}\`}}vfm`
-              node.childNodes = []
-            }
-
-            if (node.childNodes.length > 0) {
-              node.childNodes.forEach(markCodeAsPre)
-            }
-          }
+    const markCodeAsPre = (node: DomHandlerNode): void => {
+      if (node instanceof Element) {
+        if (node.tagName.match(/^[A-Z].+/)) {
+          node.tagName = `${subComponentNamespace}.${node.tagName}`
         }
-        root.forEach(markCodeAsPre)
-
-        const h = DomUtils.getOuterHTML(root).replace(/"vfm{{/g, '{{').replace(/}}vfm"/g, '}}')
-
-        const reactCode = `
-          const markdown =
-            <div>
-              ${h}
-            </div>
-        `
-        const compiledReactCode = `
-          function (props) {
-            Object.keys(props).forEach(function (key) {
-              SubReactComponent[key] = props[key]
-            })
-            ${require('@babel/core').transformSync(reactCode, { ast: false, presets: ['@babel/preset-react'] }).code}
-            return markdown
-          }
-        `
-        content.addContext(`import React from "react"\nconst ${subComponentNamespace} = {}\nconst ReactComponent = ${compiledReactCode}`)
-        content.addExporting('ReactComponent')
-      }
-
-      if (options.mode?.includes(Mode.VUE)) {
-        const root = parseDOM(html)
-
-        // Top-level <pre> tags become <pre v-pre>
-        root.forEach((node: DomHandlerNode) => {
-          if (node instanceof Element) {
-            if (['pre', 'code'].includes(node.tagName)) {
-              node.attribs['v-pre'] = 'true'
-            }
-          }
-        })
-
-        // Any <code> tag becomes <code v-pre> excepting under `<pre>`
-        const markCodeAsPre = (node: DomHandlerNode): void => {
-          if (node instanceof Element) {
-            if (node.tagName === 'code') node.attribs['v-pre'] = 'true'
-            if (node.childNodes.length > 0) node.childNodes.forEach(markCodeAsPre)
-          }
+        if (['pre', 'code'].includes(node.tagName) && node.attribs?.class) {
+          node.attribs.className = node.attribs.class
+          delete node.attribs.class
         }
-        root.forEach(markCodeAsPre)
 
-        const { code: compiledVueCode } = require('@vue/compiler-sfc').compileTemplate({ source: DomUtils.getOuterHTML(root), filename: path, id })
-        content.addContext(compiledVueCode.replace('\nexport function render(', '\nfunction vueRender(') + `\nconst VueComponent = { render: vueRender }\nVueComponent.__hmrId = ${JSON.stringify(path)}\nconst VueComponentWith = (components) => ({ components, render: vueRender })\n`)
-        content.addExporting('VueComponent')
-        content.addExporting('VueComponentWith')
-      }
+        if (node.tagName === 'code') {
+          const codeContent = DomUtils.getInnerHTML(node)
+          node.attribs.dangerouslySetInnerHTML = `vfm{{ __html: \`${codeContent.replace(/([\\`])/g, "\\$1")}\`}}vfm`
+          node.childNodes = []
+        }
 
-      return {
-        code: content.export()
+        if (node.childNodes.length > 0) {
+          node.childNodes.forEach(markCodeAsPre)
+        }
       }
     }
+    root.forEach(markCodeAsPre)
+
+    const h = DomUtils.getOuterHTML(root).replace(/"vfm{{/g, '{{').replace(/}}vfm"/g, '}}')
+
+    const reactCode = `
+      const markdown =
+        <div>
+          ${h}
+        </div>
+    `
+    const compiledReactCode = `
+      function (props) {
+        Object.keys(props).forEach(function (key) {
+          SubReactComponent[key] = props[key]
+        })
+        ${require('@babel/core').transformSync(reactCode, { ast: false, presets: ['@babel/preset-react'] }).code}
+        return markdown
+      }
+    `
+    content.addContext(`import React from "react"\nconst ${subComponentNamespace} = {}\nconst ReactComponent = ${compiledReactCode}`)
+    content.addExporting('ReactComponent')
+  }
+
+  if (options.mode?.includes(Mode.VUE)) {
+    const root = parseDOM(html)
+
+    // Top-level <pre> tags become <pre v-pre>
+    root.forEach((node: DomHandlerNode) => {
+      if (node instanceof Element) {
+        if (['pre', 'code'].includes(node.tagName)) {
+          node.attribs['v-pre'] = 'true'
+        }
+      }
+    })
+
+    // Any <code> tag becomes <code v-pre> excepting under `<pre>`
+    const markCodeAsPre = (node: DomHandlerNode): void => {
+      if (node instanceof Element) {
+        if (node.tagName === 'code') node.attribs['v-pre'] = 'true'
+        if (node.childNodes.length > 0) node.childNodes.forEach(markCodeAsPre)
+      }
+    }
+    root.forEach(markCodeAsPre)
+
+    const { code: compiledVueCode } = require('@vue/compiler-sfc').compileTemplate({ source: DomUtils.getOuterHTML(root), filename: id, id })
+    content.addContext(compiledVueCode.replace('\nexport function render(', '\nfunction vueRender(') + `\nconst VueComponent = { render: vueRender }\nVueComponent.__hmrId = ${JSON.stringify(id)}\nconst VueComponentWith = (components) => ({ components, render: vueRender })\n`)
+    content.addExporting('VueComponent')
+    content.addExporting('VueComponentWith')
+  }
+
+  return {
+    code: content.export()
   }
 }
 
 export const plugin = (options: PluginOptions = {}): Plugin => {
   return {
-    transforms: [transform(options)]
+    name: 'vite-plugin-markdown',
+    enforce: 'pre',
+    transform (code, id) {
+      return tf(code, id, options)
+    }
   }
 }
 
