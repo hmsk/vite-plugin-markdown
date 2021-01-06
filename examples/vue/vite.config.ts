f019a48af7f0e19a56850160c76a4a29b16a5cfe
hmsk <k.hamasaki@gmail.com>
2021-01-05 22:48:43 -0800

    Use vite@2 in examples/vue

--- examples/vue/vite.config.ts
+++ examples/vue/vite.config.ts
@@ -1,10 +1,11 @@
+import VuePlugin from '@vitejs/plugin-vue'
 import plugin, { Mode } from 'vite-plugin-markdown'
 
 /**
  * @type { import('vite').UserConfig }
  */
 const config = {
-  plugins: [plugin({ mode: [Mode.HTML, Mode.TOC, Mode.VUE] })]
+  plugins: [VuePlugin(), plugin({ mode: [Mode.HTML, Mode.TOC, Mode.VUE] })]
 }
 
 module.exports = config
